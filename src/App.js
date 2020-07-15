e27ea68c883ace6b0b620dea7da80afd2b60656b
Matthew Bell <matthewbell1030@gmail.com>
2020-07-15 10:56:10 -0700

    Following tutorial, added static text to index.html and, and in index.js I added text to be rendered to a dynamic div in index.html.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import logo from './logo.png';
 
 class App extends Component {
   render() {
+    
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        
       </div>
     );
   }
