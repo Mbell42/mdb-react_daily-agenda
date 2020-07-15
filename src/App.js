da4dc54a53e6b99ff42c99242a65eec5e076a82c
Matthew Bell <matthewbell1030@gmail.com>
2020-07-15 10:28:23 -0700

    Following tutorial, Removed unneccessary boilerplate comments and code. Added Hellow world to app.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header ">
-          <img src={logo} alt="logo" className="App-logo" />
-          <h1 className="App-title">Welcome to Your MDB React App</h1>
-        </header>
-        <p className="mb-2">The application is configured and ready to import our components.</p>
-        <Button href="https://mdbootstrap.com/react/" target="blank" color="light-blue"><strong>Check out our docs!</strong></Button>
+        <h1>Hello World</h1>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@
 //  REACT
 import React from 'react';
 import ReactDOM from 'react-dom';
-//  CSS
+//  CSS - font-awesome, bootstrap, mdbreact
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+//  MDB Components
+import { MDBBtn, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol} from "mdbreact";
+// CSS - main
 import './index.css';
 //  APP
 import App from './App';
-//  SERVICE WORKER
 import registerServiceWorker from './registerServiceWorker';
 
 //RENDER
