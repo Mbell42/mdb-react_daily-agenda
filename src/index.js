238c86596cf3fdeea90a8e919eb1bce910649494
Matthew Bell <matthewbell1030@gmail.com>
2020-07-15 17:06:10 -0700

    Created 'Event' Component, and added two instances of it to the left column.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,45 @@
 //DEPENDENCIES
 //  REACT
-import React, {Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component } from "react";
+import ReactDOM from "react-dom";
 //  CSS - font-awesome, bootstrap, mdbreact
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-import './index.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import "./index.css";
 //  MDB Components
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
 class App extends Component {
     state = {};
     render() {
-    return  <React.Fragment>
-                <MDBContainer>
-                    <MDBRow>
-                        <MDBCol lg="6">Left Column</MDBCol>
-                        <MDBCol lg="6">Right Column</MDBCol>
-                    </MDBRow>
-                </MDBContainer>
-            </React.Fragment>;
+        return  (
+            <React.Fragment>
+                    <MDBContainer>
+                        <MDBRow>
+                            <MDBCol md="9">
+                                <Event />
+                                <Event />
+                            </MDBCol>
+                            <MDBCol md="3"></MDBCol>
+                        </MDBRow>
+                    </MDBContainer>
+            </React.Fragment>
+        );
     }
-}
+};
+
+class Event extends Component {
+    state = {};
+    render() {
+        return (
+            <React.Fragment>
+                <h3>9:00 - Title</h3>
+            </React.Fragment>
+        );
+    }
+};
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // registerServiceWorker();
