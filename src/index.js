9efab0e1950658f395cfede3ef42e74f8ddc0f34
Matthew Bell <matthewbell1030@gmail.com>
2020-07-16 04:15:01 -0700

    Completed lesson regarding state.  Added initial state to 'Event' class, and added a button to set the state of the class.

--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ class App extends Component {
                     <MDBContainer>
                         <MDBRow>
                             <MDBCol md="9">
-                                <Event />
-                                <Event />
+                                <Event title="Meeting with George" time="10:00" />
+                                <Event title="Lunch Meeting" time="12:00" />
                             </MDBCol>
                             <MDBCol md="3"></MDBCol>
                         </MDBRow>
@@ -30,11 +30,42 @@ class App extends Component {
 };
 
 class Event extends Component {
-    state = {};
+    // state = {
+    //     time: "10:00",
+    //     title: "My state name"
+    // };
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: this.props.time,
+            title: this.props.title
+        };
+    }
     render() {
         return (
             <React.Fragment>
-                <h3>9:00 - Title</h3>
+                <h3>
+                    {this.state.time} - {this.state.title}
+                    <button
+                        onClick={() => {
+                            this.setState({title: "My NEW state title"})
+                            console.log(this.state.title);
+                        }}
+                    >
+                        Change State Title
+                    </button>
+                </h3>
+                {/* <h3>
+                    {this.state.time} - {this.state.title}
+                    <button
+                        onClick={() => {
+                            this.setState({title: "My NEW variable title"});
+                            console.log(this.varTitle);
+                        }}
+                    >
+                        Change Variable title
+                    </button>
+                </h3> */}
             </React.Fragment>
         );
     }
